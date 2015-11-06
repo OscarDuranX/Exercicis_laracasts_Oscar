0aaf6444ac4a947e187938cb5b130405716adeaa
OscarDuranX <oscarduran@iesebre.com>
2015-11-06 12:45:55 +0100

    Namespacing fet

---
+++ Namespacing-Autoloading-and-PSR4/ex.php
@@ -0,0 +1,21 @@
+<?php
+
+/*require 'src/Person.php';
+require 'src/Business.php';
+require 'src/Staff.php';*/
+
+//require 'vendor/autoload.php';
+
+use Acme\Users\Person;
+use Acme\Business;
+use Acme\Staff;
+
+$jeffrey = new Person('Jeffrey Way');
+
+$staff = new Staff([$jeffrey]);
+
+$laracasts = new Business($staff);
+
+$laracasts->hire(new Person('Jane Doe'));
+
+var_dump($laracasts->getStaffMembers());
